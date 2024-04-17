cf997f771f6601deafbaab83b3bb5434644a117d
Daniel Weber <danielcweber@users.noreply.github.com>
2024-04-17 10:57:37 +0200

    Initial commit.

---
+++ src/Repro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace OverloadResolutionRepro
+{
+    public class Foo
+    {
+        public void Method<S>(params Expression<Func<Bar, S>>[] projections) => throw new NotImplementedException();
+        public void Method<S>(params Expression<Func<Bar, Wrapper<S>>>[] projections) => throw new NotImplementedException();
+    }
+
+    public class Bar
+    {
+        public Wrapper<int> WrappedValue { get; set; } = new Wrapper<int>();
+    }
+
+    public struct Wrapper<TValue>
+    {
+    }
+
+    public class EntryPoint
+    {
+        static void Main()
+        {
+            new Foo().Method(x => x.WrappedValue);
+        }
+    }
+}
